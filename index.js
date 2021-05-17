7e2545471754fc38a9f6c680a035b20ac2d0c045
unknown <jannathsyeda7@gmail.com>
2021-05-18 01:13:41 +0600

    complete crud with node express mngo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
 
 //  const cors = require('cors');
   const bodyParser = require('body-parser');
@@ -34,19 +35,48 @@ client.connect(err => {
 
 
 
+
+
 app.post("/addProduct",(req,res)=>{
     const product=req.body;
     collection.insertOne(product)
     .then(result=>{
         console.log('data added successfully');
-        res.send('success');
+        res.redirect('/')
     })
 
 
+})
+
+
+
+app.get('/product/:id', (req, res) => {
+  productCollection.find({_id: ObjectId(req.params.id)})
+  .toArray ( (err, documents) =>{
+    res.send(documents[0]);
+  })
+})
+
+
+app.patch('/update/:id', (req, res) => {
+  collection.updateOne({_id: ObjectId(req.params.id)},
+  {
+    $set: {price: req.body.price, quantity: req.body.quantity}
+  })
+  .then (result => {
+    res.send(result.modifiedCount > 0)
+  })
+})
+
+app.delete('/delete/:id', (req, res) =>{
+  collection.deleteOne({_id: ObjectId(req.params.id)})
+  .then( result => {
+    res.send(result.deletedCount > 0);
+  })
 })
  
  
 });
 
 
-app.listen(3000,()=>console.log('Listening to port 3000'));
+app.listen(4000,()=>console.log('Listening to port 3000'));
